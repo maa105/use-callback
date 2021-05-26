fe7cbfe928087631e50505b3e574b3804677dfc7
Amin Abou Harb <maa105.aub@gmail.com>
2021-05-26 09:53:47 +0300

    feat: added example build
    to be used as github page

---
+++ docs/precache-manifest.9060f57265399038e4a4f2b2e287a009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a569ac03838f6385b1cbf12231b3adc7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42ad38acbaeffdb890cd",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "ae59e5f83fb2e23fb1d1",
+    "url": "./static/js/2.e4591210.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e4591210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ad38acbaeffdb890cd",
+    "url": "./static/js/main.47815ec2.chunk.js"
+  },
+  {
+    "revision": "315b4dc7e22854daf1f0",
+    "url": "./static/js/runtime-main.1c742b82.js"
+  }
+]);
